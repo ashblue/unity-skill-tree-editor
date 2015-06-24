4beefe38f70b5715ec2a34adbd27b8ecb806461d
Ash Blue <ash@blueashes.com>
2015-06-24 14:14:35 -0700

    Renamed files

--- Assets/Plugins/SkillTree/Core/SaveData.cs
+++ Assets/Plugins/SkillTree/Core/SaveData.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-// @TODO Probably delete this after implementing in ADNC
 namespace Adnc.SkillTree {
 	public class SaveData {
 		public string name;
