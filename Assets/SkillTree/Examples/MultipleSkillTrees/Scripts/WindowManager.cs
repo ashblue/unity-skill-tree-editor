9a9f5f590a59d1be8b979d50a9fc038819b66743
Ash Blue <ash@blueashes.com>
2015-06-24 17:05:20 -0700

    Multi-skill tree example added

---
+++ Assets/SkillTree/Examples/MultipleSkillTrees/Scripts/WindowManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Adnc.SkillTree.Example.MultiTree {
+	public class WindowManager : MonoBehaviour {
+		[SerializeField] GameObject windowListing;
+		[SerializeField] GameObject window1;
+		[SerializeField] GameObject window2;
+		[SerializeField] GameObject window3;
+
+		[SerializeField] GameObject close;
+
+		public void ShowWindow1 () {
+			window1.SetActive(true);
+			close.SetActive(true);
+			windowListing.SetActive(false);
+		}
+
+		public void ShowWindow2 () {
+			window2.SetActive(true);
+			close.SetActive(true);
+			windowListing.SetActive(false);
+		}
+
+		public void ShowWindow3 () {
+			window3.SetActive(true);
+			close.SetActive(true);
+			windowListing.SetActive(false);
+		}
+
+		public void Close () {
+			close.SetActive(false);
+			window1.SetActive(false);
+			window2.SetActive(false);
+			window3.SetActive(false);
+			windowListing.SetActive(true);
+		}
+	}
+}
+
